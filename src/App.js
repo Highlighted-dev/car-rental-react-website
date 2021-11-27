0c0d47291d0e66d7f7bbffbecfce0a197bf5772d
Highlighted-dev <bartosz.korz@gmail.com>
2021-11-27 20:21:31 +0100

    Created home page and changed title name

--- src/App.js
+++ src/App.js
@@ -3,9 +3,18 @@ import NavBar from './NavBar';
 
 function App() {
   return (
-    <div className="App">
+    <div className="app">
       <NavBar></NavBar>
-
+      <div id="home">
+        <div id="app-text-container">
+          <div id="app-main-text">
+            <h1><span>Looking</span><br></br>For a car?</h1>
+          </div>
+        </div>
+        <div id="image">
+         {/*Image*/}
+        </div>
+      </div>
     </div>
   );
 }
