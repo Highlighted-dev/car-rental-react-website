a6ca80d39a7d39ffe069cc828d65572ecfe2c793
Highlighted-dev <bartosz.korz@gmail.com>
2021-11-28 17:37:14 +0100

    Added Reviews page (Not completed)

---
+++ src/Pages/Reviews.js
@@ -0,0 +1,24 @@
+import React from 'react'
+
+const Reviews = () => {
+    return (
+        <div>
+            <h1>R</h1><br/>
+            <h1>E</h1><br/>
+            <h1>V</h1><br/>
+            <h1>I</h1><br/>
+            <h1>E</h1><br/>
+            <h1>V</h1><br/>
+            <h1>S</h1><br/>
+            <h1>R</h1><br/>
+            <h1>E</h1><br/>
+            <h1>V</h1><br/>
+            <h1>I</h1><br/>
+            <h1>E</h1><br/>
+            <h1>V</h1><br/>
+            <h1>S</h1><br/>
+        </div>
+    )
+}
+
+export default Reviews
