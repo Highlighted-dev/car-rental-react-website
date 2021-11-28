4af4ffd67aa770938b51acdf3c1b03376d29e773
Highlighted-dev <bartosz.korz@gmail.com>
2021-11-28 17:37:06 +0100

    Added about page (Not completed)

---
+++ src/Pages/About.js
@@ -0,0 +1,25 @@
+import React from 'react'
+
+const About = () => {
+    return (
+        <div>
+            <h1>A</h1><br/>
+            <h1>B</h1><br/>
+            <h1>O</h1><br/>
+            <h1>U</h1><br/>
+            <h1>T</h1><br/>
+            <h1>A</h1><br/>
+            <h1>B</h1><br/>
+            <h1>O</h1><br/>
+            <h1>U</h1><br/>
+            <h1>T</h1><br/>
+            <h1>A</h1><br/>
+            <h1>B</h1><br/>
+            <h1>O</h1><br/>
+            <h1>U</h1><br/>
+            <h1>T</h1><br/>
+        </div>
+    )
+}
+
+export default About
