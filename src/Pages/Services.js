915dc08972970a89ea07adc97f508cee40e55b63
Highlighted-dev <bartosz.korz@gmail.com>
2021-11-28 17:37:32 +0100

    Added Services page (Not completed)

---
+++ src/Pages/Services.js
@@ -0,0 +1,28 @@
+import React from 'react'
+
+const Services = () => {
+    return (
+        <div>
+            <h1>S</h1><br/>
+            <h1>E</h1><br/>
+            <h1>E</h1><br/>
+            <h1>R</h1><br/>
+            <h1>V</h1><br/>
+            <h1>I</h1><br/>
+            <h1>C</h1><br/>
+            <h1>E</h1><br/>
+            <h1>S</h1><br/>
+            <h1>S</h1><br/>
+            <h1>E</h1><br/>
+            <h1>E</h1><br/>
+            <h1>R</h1><br/>
+            <h1>V</h1><br/>
+            <h1>I</h1><br/>
+            <h1>C</h1><br/>
+            <h1>E</h1><br/>
+            <h1>S</h1><br/>
+        </div>
+    )
+}
+
+export default Services
