fc15fd9645ebe0c5e24463591b839656d42cf21e
Highlighted-dev <bartosz.korz@gmail.com>
2021-11-28 17:37:23 +0100

    Added ride page (Not completed)

---
+++ src/Pages/Ride.js
@@ -0,0 +1,26 @@
+import React from 'react'
+
+const Ride = () => {
+    return (
+        <div>
+            <h1>R</h1><br/>
+            <h1>I</h1><br/>
+            <h1>D</h1><br/>
+            <h1>E</h1><br/>
+            <h1>R</h1><br/>
+            <h1>I</h1><br/>
+            <h1>D</h1><br/>
+            <h1>E</h1><br/>
+            <h1>R</h1><br/>
+            <h1>I</h1><br/>
+            <h1>D</h1><br/>
+            <h1>E</h1><br/>
+            <h1>R</h1><br/>
+            <h1>I</h1><br/>
+            <h1>D</h1><br/>
+            <h1>E</h1><br/>
+        </div>
+    )
+}
+
+export default Ride
