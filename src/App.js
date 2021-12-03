c2e30dba56047ac4f3b37b5edea92ee90a230fe0
Highlighted-dev <bartosz.korz@gmail.com>
2021-12-03 18:55:26 +0100

    Added routes to sign in and sign up

--- src/App.js
+++ src/App.js
@@ -5,10 +5,14 @@ import Cars from './Pages/Cars';
 import Services from './Pages/Services';
 import About from './Pages/About';
 import Reviews from './Pages/Reviews';
+import SignUp from './Pages/SignUp';
+import SignIn from './Pages/SignIn';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import AuthProvider from './AuthProvider';
 
 function App() {
   return (
+  <AuthProvider>
     <div>
      <BrowserRouter>
       <NavBar />
@@ -19,9 +23,12 @@ function App() {
                 <Route exact path='/Services' element={<Services/>} />
                 <Route exact path='/About' element={<About/>} />
                 <Route exact path='/Reviews' element={<Reviews/>} />
+                <Route exact path='/SignUp' element={<SignUp/>} />
+                <Route exact path='/SignIn' element={<SignIn/>} />
             </Routes>
       </BrowserRouter>
       </div>
+    </AuthProvider>
     
   );
 }
