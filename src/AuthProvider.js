16d00c18eec8ce04d469c61587bfceb064e1fe7a
Highlighted-dev <bartosz.korz@gmail.com>
2021-12-03 18:56:15 +0100

    Completed configuration for firebase

---
+++ src/AuthProvider.js
@@ -0,0 +1,38 @@
+import React ,{useEffect, useState, useContext} from 'react'
+import { auth } from './firebase'
+
+const AuthContext = React.createContext()
+
+export function useAuth() {
+    return useContext(AuthContext)
+}
+
+const AuthProvider = ( {children}) => {
+    const [currentUser, setCurrentUser] = useState()
+
+    function signup(email,password){
+        return auth.createUserWithEmailAndPassword(email,password);
+    }
+
+    useEffect(() => {
+        const unsubscribe = auth.onAuthStateChanged(user => {
+            setCurrentUser(user);
+        })
+        return unsubscribe;
+    }, [])
+
+    const value = {
+        currentUser,
+        signup
+    }
+
+    return (
+        <div>
+            <AuthContext.Provider value={value}>
+                {children}
+            </AuthContext.Provider>
+        </div>
+    )
+}
+
+export default AuthProvider
