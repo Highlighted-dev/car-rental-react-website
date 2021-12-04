3daed86c0dfb8e01e89d8ecfa4c2ecd628aff109
Highlighted-dev <bartosz.korz@gmail.com>
2021-12-04 15:59:57 +0100

    Added styles to form

--- src/Pages/SignUp.js
+++ src/Pages/SignUp.js
@@ -1,46 +1,53 @@
 import React, {useRef, useState} from 'react'
-import { Link } from 'react-router-dom'
-import './SignUp.css'
+import { Link, useNavigate } from 'react-router-dom'
+import './SignForms.css'
 import {useAuth} from '../AuthProvider'
 const SignUp = () => {
     const emailRef = useRef()
     const passwordRef = useRef()
     const passwordConfirmRef = useRef()
-    const {signup} = useAuth()
-    //TODO Show errors on the screen
-    const [error, setError] = useState('')
+    const { signup } = useAuth()
+    const [error, setError] = useState("")
     const [loading, setLoading] = useState(false)
-
-    async function handleSubmit(e){
-        e.preventDefault();
-        if(passwordRef.current.value !== passwordConfirmRef.current.value)
-        {
-            return setError("Password do dont match")
-        }
-        try{
-            setError('')
-            setLoading(true)
-            await signup(emailRef.current.value, passwordRef.current.value);
-        }
-        catch{
-            
-            setError('Failed to create an account')
-        }
-        setLoading(false)
-        
+    const navigate = useNavigate()
+  
+    async function handleSubmit(e) {
+      e.preventDefault()
+  
+      if (passwordRef.current.value !== passwordConfirmRef.current.value) {
+        return setError("Passwords do not match")
+      }
+  
+      try {
+        setError("")
+        setLoading(true)
+        await signup(emailRef.current.value, passwordRef.current.value)
+        navigate("/");
+      } catch {
+        setError("Failed to create an account")
+      }
+  
+      setLoading(false)
     }
 
     return (
         <div id="home">
         {error && console.log(error)}
             <form onSubmit={handleSubmit}>
-                <label>E-mail:</label>
-                <input type="email" ref={emailRef} required></input>
-                <label>Password:</label>
-                <input type="password" ref={passwordRef} required></input>
-                <label>Confirm Password:</label>
-                <input type="password" ref={passwordConfirmRef} required></input>
-                <input disabled={loading} type="submit" value="Sign up"></input>
+                <h3>Sign up</h3><br />
+                <div className="input-box">
+                    <span>E-mail:</span>
+                    <input type="email" ref={emailRef} required></input>
+                </div>
+                <div className="input-box">
+                    <span>Password:</span>
+                    <input type="password" ref={passwordRef} required></input>
+                </div>
+                <div className="input-box">
+                    <span>Confirm Password:</span>
+                    <input type="password" ref={passwordConfirmRef} required></input>
+                </div>
+                <input id="btn" disabled={loading} type="submit" value="Sign up"></input><br /><br />
                 <Link to="/SignIn">Already have account?</Link>
             </form>
         </div>
