d51e07539da61090c9ef1bc7080e366b44833cd1
Highlighted-dev <bartosz.korz@gmail.com>
2021-12-09 12:20:20 +0100

    Changed about us message

--- src/Pages/js/About.js
+++ src/Pages/js/About.js
@@ -14,7 +14,7 @@ const About = () => {
                 </div>
                 <div id="about-us-text">
                     <h3>About us</h3>
-                    <label>Rentoro is proud to serve customers in the Poland. As part of the largest rental car company in the country, which owns and operates more than 3 thousand vehicles, we’re sure to have a location near you or your travel destination. And while we are an internationally recognized brand, Rentoro remains family-owned with one goal in mind for our customers: to exceed your expectations for service and overall value.</label><br /><br />
+                    <label>Rentoro is proud to serve customers in Poland. As part of the largest rental car company in the country, which owns and operates more than 3 thousand vehicles, we’re sure to have a location near you or your travel destination. And while we are an internationally recognized brand, Rentoro remains family-owned with one goal in mind for our customers: to exceed your expectations for service and overall value.</label><br /><br />
                     <label>No matter if you’re visiting for only a few days or staying for a month or more, you can count on us to provide the perfect vehicle for your transportation needs.</label><br /><br />
                     <button id="btn">Learn more</button>
                 </div>
